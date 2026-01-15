19c6304e9dee16200a15a812bd9933d5c06977e0
Chad Metcalf <metcalfc@gmail.com>
2026-01-14 21:39:13 -0800

    Fix binary naming in release archives and update Homebrew formula

--- brr.rb
+++ brr.rb
@@ -1,26 +1,36 @@
 class Brr < Formula
   desc "Terminal speed reading tool using RSVP technique"
   homepage "https://github.com/metcalfc/brr"
-  url "https://github.com/metcalfc/brr/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "" # Will be filled in after creating the release
+  version "0.1.2"
   license "MIT"
-  head "https://github.com/metcalfc/brr.git", branch: "main"
 
-  depends_on "go" => :build
+  on_macos do
+    on_intel do
+      url "https://github.com/metcalfc/brr/releases/download/v0.1.2/brr_v0.1.2_darwin_amd64.tar.gz"
+      sha256 "891b0aaeecc2ad768917a23e03437dd44f66cb975b299eb60bdf119590af14bb"
+    end
+    on_arm do
+      url "https://github.com/metcalfc/brr/releases/download/v0.1.2/brr_v0.1.2_darwin_arm64.tar.gz"
+      sha256 "d2286ffca42221634205934b26be1611501748c1c301986b3a899897cccff4a1"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/metcalfc/brr/releases/download/v0.1.2/brr_v0.1.2_linux_amd64.tar.gz"
+      sha256 "4dd1542c9658e316c897100a3e663cc9c31693f8df55e9ffd8bb5b80a3c702ae"
+    end
+    on_arm do
+      url "https://github.com/metcalfc/brr/releases/download/v0.1.2/brr_v0.1.2_linux_arm64.tar.gz"
+      sha256 "6b7beb9db6136b9edb991f839bfcc0b83cefa8949d79c8313dffa0c3b4710556"
+    end
+  end
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
-    man1.install "brr.1"
+    bin.install "brr"
   end
 
   test do
-    # Test that the binary exists and shows help
-    assert_match "Brr - Terminal Speed Reading Tool", shell_output("#{bin}/brr -h", 1)
-
-    # Test with sample text
-    (testpath/"test.txt").write("Hello world test")
-    # Can't fully test interactive features in Homebrew test environment
-    # but we can verify the binary accepts the file
-    assert_match "test.txt", shell_output("#{bin}/brr #{testpath}/test.txt 2>&1", 1)
+    assert_match "brr 0.1.2", shell_output("#{bin}/brr -v")
   end
 end
